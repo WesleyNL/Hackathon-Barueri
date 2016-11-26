82a2ed4bc3758dc39d55ec27b591e26893e5eaa6
Jefferson Felipe <felipej2626@gmail.com>
2016-11-26 20:25:30 -0200

    Inclusão das telas de Login, Cadastro, BemVindo e Seguros

---
+++ Sistema/app/src/main/java/br/com/sistema/CadastrarProposta.java
@@ -0,0 +1,13 @@
+package br.com.sistema;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastrarProposta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_proposta);
+    }
+}
