ae8c45a4e09bfb250876552d3f89fe0ebd4e14de
Jefferson Felipe <felipej2626@gmail.com>
2016-11-26 22:37:33 -0200

    Inclusão dos campos para cadastrar uma proposta

---
+++ Sistema/app/src/main/java/br/com/sistema/activity/CadastrarPropostaActivity.java
@@ -0,0 +1,27 @@
+package br.com.sistema.activity;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import br.com.sistema.R;
+
+public class CadastrarPropostaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_proposta);
+    }
+
+    public void enviarFotoFrontal(View view) {
+
+    }
+
+    private void encontrarFoto() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePicture, 0);
+    }
+}
--- Sistema/app/src/main/java/br/com/sistema/adapter/ContratosAdapter.java
+++ Sistema/app/src/main/java/br/com/sistema/adapter/ContratosAdapter.java
@@ -1,7 +1,54 @@
 package br.com.sistema.adapter;
 
+import android.preference.EditTextPreference;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import br.com.sistema.R;
+
 /**
  * Created by Jefferson on 26/11/2016.
  */
-public class ContratosAdapter {
+public class ContratosAdapter extends RecyclerView.Adapter<ContratosAdapter.ViewHolder> {
+
+    private LinkedList<Contrato> contratos;
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.frame_contratos, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(itemLayoutView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.txtID.setText(contratos.get(position).getID());
+        holder.txtNomeBem.setText(contratos.get(position).getNomeBem());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contratos.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView txtID;
+        public TextView txtNomeBem;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            txtID = (TextView) itemView.findViewById(R.id.txtID);
+            txtNomeBem = (TextView) itemView.findViewById(R.id.txtNomeBem);
+        }
+    }
 }
