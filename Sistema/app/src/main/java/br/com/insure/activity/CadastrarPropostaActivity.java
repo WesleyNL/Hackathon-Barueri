18aa81f7935319d7648ad11756584a11bfe8194e
Jefferson Felipe <felipej2626@gmail.com>
2016-11-27 00:12:59 -0200

    Inclusão da Tela de Contrato

--- Sistema/app/src/main/java/br/com/insure/activity/CadastrarPropostaActivity.java
+++ Sistema/app/src/main/java/br/com/insure/activity/CadastrarPropostaActivity.java
@@ -1,27 +1,61 @@
 package br.com.insure.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import br.com.insure.R;
+import br.com.insure.utilidades.Utils;
+import android.widget.ImageButton;
 
 public class CadastrarPropostaActivity extends AppCompatActivity {
+
+    private final int FOTO_FRONTAL = 0;
+    private final int FOTO_LATERAL = 1;
+
+    private ImageButton imgFotoFrontal;
+    private ImageButton imgFotoLateral;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_proposta);
+
+        imgFotoFrontal = (ImageButton) findViewById(R.id.imgFotoFrontal);
+        imgFotoLateral = (ImageButton) findViewById(R.id.imgFotoLateral);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case FOTO_FRONTAL:
+                if (resultCode == RESULT_OK) {
+                    imgFotoFrontal.setImageURI(data.getData());
+                }
+                break;
+
+            case FOTO_LATERAL:
+                if (resultCode == RESULT_OK) {
+                    imgFotoLateral.setImageURI(data.getData());
+                }
+                break;
+        }
     }
 
     public void enviarFotoFrontal(View view) {
-
+        encontrarFoto(FOTO_FRONTAL);
     }
 
-    private void encontrarFoto() {
-        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(takePicture, 0);
+    public void enviarFotoLateral(View view) {
+        encontrarFoto(FOTO_LATERAL);
+    }
+
+    private void encontrarFoto(int foto) {
+        startActivityForResult(Utils.getPickImageIntent(this), foto);
     }
 }
---
+++ Sistema/app/src/main/java/br/com/insure/utilidades/Utils.java
@@ -0,0 +1,59 @@
+package br.com.insure.utilidades;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.insure.R;
+
+/**
+ * Created by Jefferson on 26/11/2016.
+ */
+public class Utils {
+
+    public static Intent getPickImageIntent(Context context) {
+        Intent chooserIntent = null;
+
+        List<Intent> intentList = new ArrayList<>();
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePhotoIntent.putExtra("return-data", true);
+        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(getTempFile(context)));
+        intentList = addIntentsToList(context, intentList, pickIntent);
+        intentList = addIntentsToList(context, intentList, takePhotoIntent);
+
+        if (intentList.size() > 0) {
+            chooserIntent = Intent.createChooser(intentList.remove(intentList.size() - 1),
+                    "Escolha uma opção");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentList.toArray(new Parcelable[]{}));
+        }
+
+        return chooserIntent;
+    }
+
+    private static List<Intent> addIntentsToList(Context context, List<Intent> list, Intent intent) {
+        List<ResolveInfo> resInfo = context.getPackageManager().queryIntentActivities(intent, 0);
+        for (ResolveInfo resolveInfo : resInfo) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            Intent targetedIntent = new Intent(intent);
+            targetedIntent.setPackage(packageName);
+            list.add(targetedIntent);
+        }
+        return list;
+    }
+
+    private static File getTempFile(Context context) {
+        File imageFile = new File(context.getExternalCacheDir(), "tempImage");
+        imageFile.getParentFile().mkdirs();
+        return imageFile;
+    }
+}
